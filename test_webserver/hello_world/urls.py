fc64936961cd41b20ffb88268e0a719b5e1a65c2
Henry Atkinson <henryatkinson96@gmail.com>
2019-09-08 17:33:59 -0600

    Second Commit

--- test_webserver/hello_world/urls.py
+++ test_webserver/hello_world/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='hello_world-home'),
     path('about/', views.about, name='hello_world-about'),
+    path('map/', views.map, name='hello_world-map'),
 ]

0b53f98d75554001bf00777e11a04c254457d8b0
RyanRead <ryan.e.read@gmail.com>
2019-10-17 11:31:53 -0600

    Addition of new templates

--- test_webserver/dog_walker/static/map/view_map.js
+++ test_webserver/dog_walker/static/map/view_map.js
@@ -5,6 +5,7 @@ minZoom: 12,
 maxZoom: 18,
 });
 
+
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, { attribution });
